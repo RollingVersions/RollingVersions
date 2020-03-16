c33d73628d3c3ae465802c8710e8c188eddda579
Forbes Lindesay <forbes@lindesay.co.uk>
2020-03-16 00:04:43 +0000

    perf: use graphql to make deeply nested queries

--- packages/utils/src/GitHub.ts
+++ packages/utils/src/GitHub.ts
@@ -14,143 +14,173 @@ import isObject from './utils/isObject';
 import addVersions from './utils/addVersions';
 import VersionTag from './VersionTag';
 
+import Client from '@github-graph/api';
+import * as gh from './github-graph';
+
+async function* paginate<TPage, TEntry>(
+  getPage: (token?: string) => Promise<TPage>,
+  getEntries: (page: TPage) => TEntry[],
+  getNextPageToken: (page: TPage) => string | undefined,
+) {
+  let page;
+  let nextPageToken;
+  while (nextPageToken) {
+    page = await getPage(nextPageToken);
+    nextPageToken = getNextPageToken(page);
+    for (const entry of getEntries(page)) {
+      yield entry;
+    }
+  }
+}
 export async function getAllTags(
-  github: Pick<Octokit, 'repos'>,
+  client: Client,
   pr: Pick<PullRequst, 'owner' | 'repo'>,
 ) {
   const results: Pick<VersionTag, 'commitSha' | 'name'>[] = [];
-  let page = 1;
-  let nextPage = true;
-  while (nextPage) {
-    const data = (
-      await github.repos.listTags({
-        owner: pr.owner,
-        repo: pr.repo,
-        per_page: 100,
-        page,
-      })
-    ).data.map((t) => ({name: t.name, commitSha: t.commit.sha}));
-    results.push(...data);
-    nextPage = data.length === 100;
-    page++;
+  for await (const tag of paginate(
+    (after) => gh.getTags(client, {owner: pr.owner, name: pr.repo, after}),
+    (page) => page?.repository?.refs?.nodes || [],
+    (page) =>
+      (page?.repository?.refs?.pageInfo?.hasNextPage &&
+        page?.repository?.refs?.pageInfo?.endCursor) ||
+      undefined,
+  )) {
+    if (tag) {
+      results.push({commitSha: tag.target.oid, name: tag.name});
+    }
   }
   return results;
 }
+
+interface Entry {
+  name: string;
+  object:
+    | null
+    | {__typename: 'Tree'; oid: string; entries?: Entry[] | null}
+    | {__typename: 'Blob'; oid: string}
+    | {__typename: 'Tag'}
+    | {__typename: 'Commit'};
+}
 async function listRawPackages(
-  github: Pick<Octokit, 'repos'>,
-  pr: Pick<PullRequst, 'owner' | 'repo' | 'headSha'>,
+  client: Client,
+  pr: Pick<PullRequst, 'owner' | 'repo' | 'number'>,
 ) {
+  const commit = (
+    await gh.getPullRequestFileNames(client, {
+      owner: pr.owner,
+      name: pr.repo,
+      number: pr.number,
+    })
+  ).repository?.pullRequest?.headRef?.target;
+  if (!commit || commit.__typename !== 'Commit') {
+    throw new Error(
+      `Expected a Commit but got ${commit?.__typename || 'undefined'}`,
+    );
+  }
+
   const packages = new Map<
     string,
     Array<Omit<PackageInfo, 'versionTag' | 'registryVersion'>>
   >();
-  let ref: string | undefined = pr.headSha;
-  let root: Octokit.Response<Octokit.ReposGetContentsResponse>;
-  try {
-    root = await github.repos.getContents({
-      owner: pr.owner,
-      repo: pr.repo,
-      ref,
-      path: '',
-    });
-  } catch (ex) {
-    root = await github.repos.getContents({
-      owner: pr.owner,
-      repo: pr.repo,
-      path: '',
-    });
-    ref = undefined;
-  }
-
-  async function walk(item: Octokit.ReposGetContentsResponseItem) {
-    if (
-      item.type === 'file' &&
-      item.path !== 'package.json' &&
-      !item.path.endsWith('/package.json')
-    ) {
-      return;
-    }
-    const entry = await github.repos.getContents({
-      owner: pr.owner,
-      repo: pr.repo,
-      ref,
-      path: item.path,
-    });
-    if (Array.isArray(entry.data)) {
-      for (const item of entry.data) {
-        await walk(item);
-      }
-    } else {
-      if (item.path === 'package.json' || item.path.endsWith('/package.json')) {
-        const content = entry.data.content
-          ? Buffer.from(entry.data.content, 'base64').toString('utf8')
-          : '';
-        let result: unknown;
-        try {
-          result = JSON.parse(content);
-        } catch (ex) {
-          // ignore
+  const queue: Promise<unknown>[] = [];
+  function processEntry(entry: Entry, path: string[]) {
+    switch (entry.object?.__typename) {
+      case 'Blob':
+        if (entry.name === 'package.json') {
+          queue.push(
+            gh
+              .getFile(client, {
+                owner: pr.owner,
+                name: pr.repo,
+                oid: entry.object.oid,
+              })
+              .then((file) => {
+                if (file.repository?.object?.__typename === 'Blob') {
+                  const content = file.repository.object.text;
+                  let result: unknown;
+                  try {
+                    result = content && JSON.parse(content);
+                  } catch (ex) {
+                    // ignore
+                  }
+                  if (isObject(result) && typeof result.name === 'string') {
+                    packages.set(result.name, packages.get(result.name) || []);
+                    packages.get(result.name)!.push({
+                      path: [...path, entry.name].join('/'),
+                      platform: Platform.npm,
+                      publishConfigAccess:
+                        result.name[0] === '@'
+                          ? isObject(result.publishConfig) &&
+                            result.publishConfig.access === 'public'
+                            ? 'public'
+                            : 'restricted'
+                          : 'public',
+                      packageName: result.name,
+                      notToBePublished: result.private === true,
+                    });
+                  }
+                }
+              }),
+          );
         }
-        if (isObject(result) && typeof result.name === 'string') {
-          packages.set(result.name, packages.get(result.name) || []);
-          packages.get(result.name)!.push({
-            path: entry.data.path.replace(/^\//, ''),
-            platform: Platform.npm,
-            publishConfigAccess:
-              result.name[0] === '@'
-                ? isObject(result.publishConfig) &&
-                  result.publishConfig.access === 'public'
-                  ? 'public'
-                  : 'restricted'
-                : 'public',
-            packageName: result.name,
-            notToBePublished: result.private === true,
-          });
-        }
-      }
+        break;
+      case 'Tree':
+        entry.object.entries?.forEach((e) =>
+          processEntry(e, [...path, entry.name]),
+        );
+        break;
     }
   }
-  if (Array.isArray(root.data)) {
-    for (const item of root.data) {
-      await walk(item);
-    }
-  } else {
-    await walk(root.data);
-  }
-
+  commit.tree.entries?.forEach((entry) => processEntry(entry, []));
+  await Promise.all(queue);
   return packages;
 }
 export async function listPackages(
-  github: Pick<Octokit, 'repos'>,
-  pr: Pick<PullRequst, 'owner' | 'repo' | 'headSha'>,
+  client: Client,
+  pr: Pick<PullRequst, 'owner' | 'repo' | 'number'>,
 ): Promise<PackageInfos> {
   const [packages, allTags] = await Promise.all([
-    listRawPackages(github, pr),
-    getAllTags(github, pr),
+    listRawPackages(client, pr),
+    getAllTags(client, pr),
   ]);
 
   return await addVersions(packages, allTags);
 }
 
 export async function readComment(
-  github: Pick<Octokit, 'issues'>,
+  client: Client,
   pr: Pick<PullRequst, 'owner' | 'repo' | 'number'>,
 ): Promise<{
   existingComment: number | undefined;
   state: PullChangeLog;
 }> {
-  const comments = await github.issues.listComments({
-    issue_number: pr.number,
-    owner: pr.owner,
-    repo: pr.repo,
-    per_page: 20,
-  });
-  const existingComment = comments.data.find((comment) =>
-    comment.body.includes(COMMENT_GUID),
-  );
+  for await (const comment of paginate(
+    (after) =>
+      gh.getPullRequestComments(client, {
+        owner: pr.owner,
+        name: pr.repo,
+        number: pr.number,
+        after,
+      }),
+    (page) => page?.repository?.pullRequest?.comments?.nodes || [],
+    (page): string | undefined =>
+      (page?.repository?.pullRequest?.comments?.pageInfo?.hasNextPage &&
+        page?.repository?.pullRequest?.comments?.pageInfo?.endCursor) ||
+      undefined,
+  )) {
+    if (comment?.databaseId && comment?.body.includes(COMMENT_GUID)) {
+      return {
+        existingComment: comment.databaseId,
+        state: readState(comment.body) || {
+          submittedAtCommitSha: null,
+          packages: [],
+        },
+      };
+    }
+  }
   return {
-    existingComment: existingComment?.id,
-    state: readState(existingComment?.body) || {
+    existingComment: undefined,
+    state: {
       submittedAtCommitSha: null,
       packages: [],
     },
