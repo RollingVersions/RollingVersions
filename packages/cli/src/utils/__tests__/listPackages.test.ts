cc7e8994c79da51de4f0389e016b4d9b1d3c46d9
Forbes Lindesay <forbes@lindesay.co.uk>
2020-04-17 12:29:08 +0100

    feat: allow ignoring a package from rollingversions (#43)

--- packages/cli/src/utils/__tests__/listPackages.test.ts
+++ packages/cli/src/utils/__tests__/listPackages.test.ts
@@ -50,3 +50,84 @@ test('listPackages - single npm package', async () => {
     }
   `);
 });
+
+test('listPackages - multiple npm packages', async () => {
+  const {dirname, newCommit} = createRepository({
+    files: [
+      {
+        path: 'package.json',
+        contents: '{"name": "root-package", "@rollingversions/ignore": true}',
+      },
+      {
+        path: 'package.json',
+        contents: '{"name": "@root-package/a"}',
+      },
+      {
+        path: 'package.json',
+        contents: '{"name": "@root-package/b"}',
+      },
+    ],
+  });
+  expect(await listPackages(getAllTags(dirname), getAllFiles(dirname)))
+    .toMatchInlineSnapshot(`
+    Map {
+      "@root-package/a" => Array [
+        Object {
+          "notToBePublished": false,
+          "packageName": "@root-package/a",
+          "path": "package.json",
+          "publishConfigAccess": "restricted",
+          "publishTarget": "npm",
+          "registryVersion": null,
+          "versionTag": null,
+        },
+      ],
+      "@root-package/b" => Array [
+        Object {
+          "notToBePublished": false,
+          "packageName": "@root-package/b",
+          "path": "package.json",
+          "publishConfigAccess": "restricted",
+          "publishTarget": "npm",
+          "registryVersion": null,
+          "versionTag": null,
+        },
+      ],
+    }
+  `);
+  newCommit({tags: ['@root-package/a@1.0.0']});
+  newCommit({tags: ['@root-package/a@2.0.0']});
+  newCommit({tags: ['@root-package/a@3.0.0']});
+  createNpmPackage('@root-package/a', {versions: ['2.0.0']});
+  expect(await listPackages(getAllTags(dirname), getAllFiles(dirname)))
+    .toMatchInlineSnapshot(`
+    Map {
+      "@root-package/a" => Array [
+        Object {
+          "notToBePublished": false,
+          "packageName": "@root-package/a",
+          "path": "package.json",
+          "publishConfigAccess": "restricted",
+          "publishTarget": "npm",
+          "registryVersion": "2.0.0",
+          "versionTag": Object {
+            "commitSha": "COMMIT_SHA_2",
+            "name": "@root-package/a@2.0.0",
+            "version": "2.0.0",
+          },
+        },
+      ],
+      "@root-package/b" => Array [
+        Object {
+          "notToBePublished": false,
+          "packageName": "@root-package/b",
+          "path": "package.json",
+          "publishConfigAccess": "restricted",
+          "publishTarget": "npm",
+          "registryVersion": null,
+          "versionTag": null,
+        },
+      ],
+    }
+  `);
+});
