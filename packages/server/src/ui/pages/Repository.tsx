21ad7704fab7693bc5d550f74609f1c864faea92
Forbes Lindesay <forbes@lindesay.co.uk>
2020-04-29 19:47:10 +0100

    fix: parameter order

--- packages/server/src/ui/pages/Repository.tsx
+++ packages/server/src/ui/pages/Repository.tsx
@@ -89,7 +89,7 @@ export default function Repository() {
       })}
       <form
         method="POST"
-        action={`/${params.repo}/${params.owner}/dispatch/rollingversions_publish_approved`}
+        action={`/${params.owner}/${params.repo}/dispatch/rollingversions_publish_approved`}
       >
         <button type="submit">Release These Changes</button>
       </form>
