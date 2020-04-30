bdb2143856513df4a1592691cb200f311acf25b8
Forbes Lindesay <forbes@lindesay.co.uk>
2020-04-30 22:20:52 +0100

    fix: storybook type error

--- packages/server/src/ui/visual/HeroBar/index.stories.tsx
+++ packages/server/src/ui/visual/HeroBar/index.stories.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
-import HeroBar, {HeroBarFooter} from './';
+import HeroBar, {HeroBarFooter, InstallButton} from './';
 
 export default {title: 'modules/HeroBar'};
 
 export const Default = () => {
-  return <HeroBar />;
+  return (
+    <HeroBar callToAction={<InstallButton />}>
+      <p>Add change sets to pull requests</p>
+      <p>Automatically release with change logs</p>
+    </HeroBar>
+  );
 };
 export const Footer = () => {
   return <HeroBarFooter />;
