4a0be8ebab0165adc3cf66de5f0547756c91bc5b
Forbes Lindesay <forbes@lindesay.co.uk>
2020-05-19 00:49:41 +0100

    refactor: simplify pagination and add error logs (#74)

--- packages/cli/src/services/github/paginate.ts
+++ packages/cli/src/services/github/paginate.ts
@@ -1,25 +1,17 @@
+import paginateBatched from './paginateBatched';
+
 export default async function* paginate<TPage, TEntry>(
   getPage: (token?: string) => Promise<TPage>,
   getEntries: (page: TPage) => TEntry[],
   getNextPageToken: (page: TPage) => string | undefined,
 ) {
-  let nextPage: Promise<TPage> | undefined;
-  let currentPage: TPage | undefined;
-  let nextPageToken: string | undefined;
-  do {
-    nextPage = getPage(nextPageToken);
-    nextPage.catch(() => {
-      // swallow errors here because otherwise they can be
-      // reported as unhandled exceptions before we get to
-      // the part where we await this promise
-    });
-    for (const entry of currentPage ? getEntries(currentPage) : []) {
-      yield entry;
+  for await (const page of paginateBatched(
+    getPage,
+    getEntries,
+    getNextPageToken,
+  )) {
+    for (const item of page) {
+      yield item;
     }
-    currentPage = await nextPage;
-    nextPageToken = getNextPageToken(currentPage);
-  } while (nextPageToken);
-  for (const entry of currentPage ? getEntries(currentPage) : []) {
-    yield entry;
   }
 }
