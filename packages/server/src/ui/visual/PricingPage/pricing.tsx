3d7627363a06c5287daaa1816b538c0948460c5b
Dee <dee@lindesay.co.uk>
2020-05-27 18:01:16 +0100

    overlaps

--- packages/server/src/ui/visual/PricingPage/pricing.tsx
+++ packages/server/src/ui/visual/PricingPage/pricing.tsx
@@ -1,27 +1,79 @@
 import React from 'react';
 
+function OptionBox({
+  optionName,
+  benefits,
+  className,
+}: {
+  optionName: string;
+  benefits: string[];
+  className?: string;
+}) {
+  return (
+    <div
+      className={`mx-3 w-64 border-orange-500  bg-white font-poppins font-normal text-3xl text-gray-900 ${className ||
+        ''}`}
+    >
+      <p className="text-center">{optionName}</p>
+      <ul className="font-sans text-xl list-square ml-6">
+        {benefits.map((benefit) => (
+          <li>{benefit}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export default function Pricing() {
   return (
     <div>
       <div className="bg-gray-200 mx-12">
-        <div className="w-screen h-auto overflow-visible">
+        <div className="w-screen h-auto">
           <div
-            className="bg-black h-56 -mx-12"
+            className="bg-gray-900 h-64 -mx-12"
             style={{
-              clipPath: 'polygon(0 0, 100% 0, 100% 81.25%, 0 100%)',
+              clipPath: 'polygon(0 0, 100% 0, 100% 80%, 0 100%)',
             }}
+          ></div>
+          <div
+            className="flex justify-center pt-12"
+            style={{position: 'relative', marginTop: '-220px'}}
           >
-            <div className="container mx-auto flex justify-center pt-12 overflow-visible">
-              <div className="mt-12 mx-4 h-56 w-56 border border-orange-500  bg-white max-w-4xl font-poppins font-normal text-3xl text-white italic">
-                <p className="text-gray-900 text-center">Hobby</p>
-              </div>
-              <div className="h-64 w-56 border border-4 border-orange-500  bg-white max-w-4xl font-poppins font-normal text-3xl text-white italic">
-                <p className="text-gray-900 text-center">Teams</p>
-              </div>
-              <div className="mt-12 h-56 w-56 mx-4 border border-orange-500  bg-white max-w-4xl font-poppins font-normal text-3xl text-white italic">
-                <p className="text-gray-900 text-center">Enterprise</p>
-              </div>
-            </div>
+            <OptionBox
+              className="mt-12 border"
+              optionName="Hobby"
+              benefits={[
+                'Individual projects',
+                'Working with a friend',
+                'Easy set-up',
+                'Fully hosted',
+                'Unlimited repositories',
+                'Community support',
+              ]}
+            />
+            <OptionBox
+              className="border-4"
+              optionName="Teams"
+              benefits={[
+                'Perfect for teams of 3 or more',
+                'Integrates with github teams',
+                'Easy set-up',
+                'Fully hosted',
+                'Unlimited repositories',
+                'Community support',
+              ]}
+            />
+            <OptionBox
+              className="mt-12 border"
+              optionName="Enterprise"
+              benefits={[
+                'Unlimited users',
+                'Self-hosted',
+                'Integrates with github enterprise',
+                'Unlimited repositories',
+                'Tailored support',
+              ]}
+            />
           </div>
         </div>
         <div className="flex justify-center py-12">
