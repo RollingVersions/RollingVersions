bd058023a3f7acf92e9bc0a6730a643ca86c78ea
Dee <dee@lindesay.co.uk>
2020-05-28 10:47:36 +0100

    add a title

--- packages/server/src/ui/visual/PricingPage/pricing.tsx
+++ packages/server/src/ui/visual/PricingPage/pricing.tsx
@@ -32,12 +32,17 @@ export default function Pricing() {
           <div
             className="bg-gray-900 h-64 sm:-mx-8 lg:-mx-12 xl:-mx-32"
             style={{
+              height: '20rem',
               clipPath: 'polygon(0 0, 100% 0, 100% 80%, 0 100%)',
             }}
-          ></div>
+          >
+            <p className="font-poppins font-normal text-orange-500 text-4xl lg:text-5xl py-6 lg:py-4 text-center">
+              Pricing
+            </p>
+          </div>
           <div
             className="mx-4 sm:mx-8 lg:mx-12 xl:mx-32 grid grid-cols-1 md:grid-cols-3 gap-4 xl:gap-12"
-            style={{position: 'relative', marginTop: '-200px'}}
+            style={{position: 'relative', marginTop: '-220px'}}
           >
             <OptionBox
               className="mx-4 md:mx-0 md:my-12 border"
