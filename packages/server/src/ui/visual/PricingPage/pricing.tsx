e3adf8a3890393c239551cfb43494442a01d09af
Dee <dee@lindesay.co.uk>
2020-05-30 13:19:30 +0100

    expanded "white" space

--- packages/server/src/ui/visual/PricingPage/pricing.tsx
+++ packages/server/src/ui/visual/PricingPage/pricing.tsx
@@ -4,25 +4,25 @@ import OptionBox from './optionBox';
 export default function Pricing() {
   return (
     <div>
-      <div className="bg-gray-200 sm:mx-8 lg:mx-12 xl:mx-32">
+      <div className="bg-gray-200 mx-8 sm:mx-12 md:mx-8 lg:mx-12 xl:mx-32">
         <div className="h-auto">
           <div
-            className="bg-gray-900 h-64 sm:-mx-8 lg:-mx-12 xl:-mx-32"
+            className="bg-gray-900 -mx-8 sm:-mx-12 mx:mx-8 lg:-mx-12 xl:-mx-32"
             style={{
-              height: '20rem',
+              height: '22rem',
               clipPath: 'polygon(0 0, 100% 0, 100% 80%, 0 100%)',
             }}
           >
-            <h1 className="font-poppins font-normal text-orange-500 text-4xl lg:text-5xl py-6 lg:py-4 text-center">
+            <h1 className="font-poppins font-normal text-orange-500 text-4xl md:text-5xl py-12 md:py-14 lg:py-12 text-center">
               Pricing
             </h1>
           </div>
           <div
             className="mx-4 sm:mx-8 lg:mx-12 xl:mx-32 grid grid-cols-1 md:grid-cols-3 gap-4 xl:gap-12"
-            style={{position: 'relative', marginTop: '-220px'}}
+            style={{position: 'relative', marginTop: '-185px'}}
           >
             <OptionBox
-              className="mx-4 md:mx-0 md:my-12 border"
+              className="mx-4 sm:mx-12 md:mx-0 md:my-12 border"
               optionName="Hobby"
               benefits={[
                 'Individual projects',
@@ -34,7 +34,7 @@ export default function Pricing() {
               ]}
             />
             <OptionBox
-              className="border-4"
+              className="sm:mx-4 md:mx-0 border-4"
               optionName="Teams"
               benefits={[
                 'Perfect for teams of 3 or more',
@@ -46,7 +46,7 @@ export default function Pricing() {
               ]}
             />
             <OptionBox
-              className="mx-4 md:mx-0 md:my-12 border"
+              className="mx-4 sm:mx-12 md:mx-0 md:my-12 border"
               optionName="Enterprise"
               benefits={[
                 'Unlimited users',
