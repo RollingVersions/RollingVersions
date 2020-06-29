b4e245ce652fd471b5146f7c7d012998bf516d4c
Forbes Lindesay <forbes@lindesay.co.uk>
2020-06-29 19:21:43 +0100

    feat: improve landing page language

--- packages/server/src/ui/visual/LandingPage/index.tsx
+++ packages/server/src/ui/visual/LandingPage/index.tsx
@@ -6,7 +6,7 @@ export default function LandingPage() {
   return (
     <>
       <HeroBar callToAction={<InstallButton />}>
-        <p>Add change sets to pull requests</p>
+        <p>Add change sets to your pull requests</p>
         <p>Automatically release with change logs</p>
       </HeroBar>
       <div className="py-8 xs:py-16">
