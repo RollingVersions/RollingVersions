a54cfc2f4a05a8c8793f7de5efdc92b050f8109d
Forbes Lindesay <forbes@lindesay.co.uk>
2020-09-18 18:03:45 +0100

    chore: update funtypes to get better error messages

--- packages/server/src/server/middleware/utils/validateBody.ts
+++ packages/server/src/server/middleware/utils/validateBody.ts
@@ -1,22 +1,14 @@
 import {Response, Request} from 'express';
+import {showError} from 'funtypes';
 import {UpdatePullRequestBody} from '../../../types';
 
 export default function validateBody() {
   return (req: Request, res: Response, next: (err?: any) => void) => {
     const result = UpdatePullRequestBody.safeParse(req.body);
     if (result.success) next();
-    else
-      res
-        .status(400)
-        .send(
-          `${
-            result.key ? `${result.message} in ${result.key}` : result.message
-          }\n`,
-        );
+    else res.status(400).send(`${showError(result)}\n`);
   };
 }
 export function getBody(req: Request): UpdatePullRequestBody {
-  const result = UpdatePullRequestBody.safeParse(req.body);
-  if (result.success) return result.value;
-  else throw new Error(result.message);
+  return UpdatePullRequestBody.parse(req.body);
 }
